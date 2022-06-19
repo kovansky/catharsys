99c6cd9ca552b3c6fe1007ccc2b9c4b46b6e48ba
F4 Developer <skowanski@f4dev.me>
2022-06-19 20:16:21 +0200

    rm function

--- js/getRandomThought.js
+++ js/getRandomThought.js
@@ -3,72 +3,68 @@ const api = axios.create({
 })
 
 
-const randomThought = () => {
-    api.get('/thoughts/random')
-        .then((response) => {
-            if(!response.data.error) {
-                const thought = response.data.data // Some random thought
+api.get('/thoughts/random')
+    .then((response) => {
+        if(!response.data.error) {
+            const thought = response.data.data // Some random thought
 
-                console.log(`Random thought with text "${thought.text}"`)
+            console.log(`Random thought with text "${thought.text}"`)
 
-                let messagesInnerBlocks = document.querySelector(".messages__inner-blocks")
-                messagesInnerBlocks.dataset.thoughtId = thought.id;
-                let messagesInnerBlocksBlock = document.createElement("div")
-                messagesInnerBlocksBlock.classList.add("messages__inner-blocks-block")
-                
-                let messagesUser = document.createElement("div")
-                messagesUser.classList.add("messages__inner-blocks-block-user")
-                let messageText = document.createElement("div")
-                messageText.classList.add("messages__inner-blocks-block-text")
-                let messageReactions = document.createElement("div")
-                messageReactions.classList.add("messages__inner-blocks-block-reactions")
-
-
-                let messageUserImg = document.createElement("img")
-                messageUserImg.src = "img/user.svg"
-
-                let messageTextH = document.createElement("h1")
-                messageTextH.innerHTML = thought.text
-
-                let messageReactionsHeart = document.createElement("div")
-                messageReactionsHeart.classList.add("messages__inner-blocks-block-reactions-heart")
-                let messageReactionsHeartH = document.createElement("h1")
-                messageReactionsHeartH.innerHTML = thought.hugs;
-                messageReactionsHeartH.classList.add("messages__inner-blocks-block-reactions-heart-counter")
-                let messageReactionsHeartImg = document.createElement("img")
-                messageReactionsHeartImg.src = "img/heart.png"
-
-                let messageReactionsLike = document.createElement("div")
-                messageReactionsLike.classList.add("messages__inner-blocks-block-reactions-like")
-                let messageReactionsLikeH = document.createElement("h1")
-                messageReactionsLikeH.innerHTML = thought.relatables;
-                messageReactionsLikeH.classList.add("messages__inner-blocks-block-reactions-like-counter")
-                let messageReactionsLikeImg = document.createElement("img")
-                messageReactionsLikeImg.src = "img/like.png"
-                
+            let messagesInnerBlocks = document.querySelector(".messages__inner-blocks")
+            messagesInnerBlocks.dataset.thoughtId = thought.id;
+            let messagesInnerBlocksBlock = document.createElement("div")
+            messagesInnerBlocksBlock.classList.add("messages__inner-blocks-block")
+            
+            let messagesUser = document.createElement("div")
+            messagesUser.classList.add("messages__inner-blocks-block-user")
+            let messageText = document.createElement("div")
+            messageText.classList.add("messages__inner-blocks-block-text")
+            let messageReactions = document.createElement("div")
+            messageReactions.classList.add("messages__inner-blocks-block-reactions")
 
 
-                messagesInnerBlocks.appendChild(messagesInnerBlocksBlock)
-                messagesInnerBlocksBlock.appendChild(messagesUser)
-                messagesUser.appendChild(messageUserImg)
+            let messageUserImg = document.createElement("img")
+            messageUserImg.src = "img/user.svg"
 
-                messagesInnerBlocksBlock.appendChild(messageText)
-                messageText.appendChild(messageTextH)
+            let messageTextH = document.createElement("h1")
+            messageTextH.innerHTML = thought.text
 
-                messagesInnerBlocksBlock.appendChild(messageReactions)
-                messageReactions.appendChild(messageReactionsHeart)
-                messageReactionsHeart.appendChild(messageReactionsHeartImg)
-                messageReactionsHeart.appendChild(messageReactionsHeartH)
+            let messageReactionsHeart = document.createElement("div")
+            messageReactionsHeart.classList.add("messages__inner-blocks-block-reactions-heart")
+            let messageReactionsHeartH = document.createElement("h1")
+            messageReactionsHeartH.innerHTML = thought.hugs;
+            messageReactionsHeartH.classList.add("messages__inner-blocks-block-reactions-heart-counter")
+            let messageReactionsHeartImg = document.createElement("img")
+            messageReactionsHeartImg.src = "img/heart.png"
 
-                messagesInnerBlocksBlock.appendChild(messageReactions)
-                messageReactions.appendChild(messageReactionsLike)
-                messageReactionsLike.appendChild(messageReactionsLikeImg)
-                messageReactionsLike.appendChild(messageReactionsLikeH)
-            }
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-}
+            let messageReactionsLike = document.createElement("div")
+            messageReactionsLike.classList.add("messages__inner-blocks-block-reactions-like")
+            let messageReactionsLikeH = document.createElement("h1")
+            messageReactionsLikeH.innerHTML = thought.relatables;
+            messageReactionsLikeH.classList.add("messages__inner-blocks-block-reactions-like-counter")
+            let messageReactionsLikeImg = document.createElement("img")
+            messageReactionsLikeImg.src = "img/like.png"
+            
 
-randomThought();
+
+            messagesInnerBlocks.appendChild(messagesInnerBlocksBlock)
+            messagesInnerBlocksBlock.appendChild(messagesUser)
+            messagesUser.appendChild(messageUserImg)
+
+            messagesInnerBlocksBlock.appendChild(messageText)
+            messageText.appendChild(messageTextH)
+
+            messagesInnerBlocksBlock.appendChild(messageReactions)
+            messageReactions.appendChild(messageReactionsHeart)
+            messageReactionsHeart.appendChild(messageReactionsHeartImg)
+            messageReactionsHeart.appendChild(messageReactionsHeartH)
+
+            messagesInnerBlocksBlock.appendChild(messageReactions)
+            messageReactions.appendChild(messageReactionsLike)
+            messageReactionsLike.appendChild(messageReactionsLikeImg)
+            messageReactionsLike.appendChild(messageReactionsLikeH)
+        }
+    })
+    .catch((error) => {
+        console.error(error)
+    })
